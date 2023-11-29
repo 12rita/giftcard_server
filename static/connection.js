2e33089b356e86d2015cff2706a7a13ea4da9f6c
Margo <m.goncharova@group-ib.com>
2023-11-29 20:53:25 +0700

    gitignore added

---
+++ static/connection.js
@@ -0,0 +1,17 @@
+import pgk from "pg"
+import dotenv from "dotenv"
+dotenv.config()
+
+
+const  { Client, Pool } = pgk;
+export const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT
+})
+
+
+
+
